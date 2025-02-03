b4baa7ef8ca565e93354099f084f884ce603e6ad
Munish <hmunish5@gmail.com>
2025-02-03 10:10:38 +0530

    Setup app component & global routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from './features/loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoadingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'framework';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './features/auth/auth.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { AnalyticsComponent } from './features/analytics/analytics.component';
+import { NavbarComponent } from './features/navbar/navbar.component';
+import { NewsletterComponent } from './features/widgets/newsletter/newsletter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'signup',
+    component: AuthComponent,
+  },
+  {
+    path: 'signin',
+    component: AuthComponent,
+  },
+  {
+    path: 'analytics',
+    component: NavbarComponent,
+    children: [{ path: '', component: AnalyticsComponent }],
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'widgets',
+    component: NavbarComponent,
+    children: [{ path: 'newsletter', component: NewsletterComponent }],
+    canActivate: [AuthGuard],
+  },
+  { path: '**', redirectTo: 'analytics' },
+];
