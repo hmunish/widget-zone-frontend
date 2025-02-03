1baf4a789114080b9a5080f47d5effcbf93c2ff6
Munish <hmunish5@gmail.com>
2025-02-03 12:39:57 +0530

    Add global loader service

---
+++ src/app/core/services/loading.service.ts
@@ -0,0 +1,18 @@
+// src/app/services/loading.service.ts
+import { Injectable, Signal, WritableSignal, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoadingService {
+  private loadingState: WritableSignal<boolean> = signal(false);
+  loading$: Signal<boolean> = this.loadingState.asReadonly();
+
+  show() {
+    this.loadingState.set(true);
+  }
+
+  hide() {
+    this.loadingState.set(false);
+  }
+}
