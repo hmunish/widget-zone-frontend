d9272ce091efca4b3d469683509675839726e4f1
Munish <hmunish5@gmail.com>
2025-02-11 21:25:17 +0530

    Update base API url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseAPIUrl: 'https://widget-zone-backend-production.up.railway.app',
+  baseAPIUrl: 'https://widget-zone-backend-production.up.railway.app:3000',
   apiCallingTime: 1000 * 120,
 };
