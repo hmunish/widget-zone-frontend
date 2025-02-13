9eaa50f9c3a935ca8809844c959f2a355a317a84
Munish <hmunish5@gmail.com>
2025-02-13 18:13:44 +0530

    Fix navbar component UI

--- src/app/features/navbar/navbar.component.ts
+++ src/app/features/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { AuthenticationService } from '../../core/services/auth.service';
+import { CurrentUser } from '../../core/interfaces/common.interface';
 
 @Component({
   selector: 'app-navbar',
@@ -10,9 +11,13 @@ import { AuthenticationService } from '../../core/services/auth.service';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  currentUser: CurrentUser | null = null;
   constructor(private authService: AuthenticationService) {}
 
+  ngOnInit(): void {
+    this.currentUser = this.authService.getCurrentUser();
+  }
   handleSignout() {
     this.authService.logout();
   }
